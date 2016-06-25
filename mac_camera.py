f6a88bf3ba247ef6299bc6a183390f8a622a2345
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-06-25 19:05:52 -0400

    successfully saves a video file

--- mac_camera.py
+++ mac_camera.py
@@ -6,6 +6,10 @@ vc = cv2.VideoCapture(0)
 
 rval, frame = vc.read()
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1280,720))
+#out = cv2.VideoWriter('video.avi',-1,1,(width,height))
+
 while True:
 
   now = datetime.now()
@@ -13,6 +17,14 @@ while True:
   if frame is not None:   
      cv2.imshow("preview", frame)
   rval, frame = vc.read()
+
+  # Use the code below if I need find the dimensions of the video
+  '''
+  height, width, channels = frame.shape
+  print(height)
+  print(width)
+  '''
+  out.write(frame)
 
   if cv2.waitKey(1) & 0xFF == ord('q'):
      break
