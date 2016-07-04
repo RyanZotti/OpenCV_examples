954859a501face4b9a1e1d395537333e963375d6
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-07-03 21:39:29 -0400

    nearly working basic overlay

--- overlay_image.py
+++ overlay_image.py
@@ -11,8 +11,20 @@ args = vars(ap.parse_args())
 mov_file = args["file"]
 
 up_arrow = cv2.imread('/Users/ryanzotti/Dropbox/PhotoshopMiscellaneous/UpArrow.tif')
+all_arrows = cv2.imread('/Users/ryanzotti/Dropbox/PhotoshopMiscellaneous/All Arrows.tif')
 scale = 0.125
 resized_image = cv2.resize(up_arrow,None,fx=scale, fy=scale, interpolation = cv2.INTER_CUBIC)
+img2gray = cv2.cvtColor(resized_image,cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+thing1 = cv2.bitwise_and(resized_image,resized_image,mask = mask_inv)
+rows,cols,channels = resized_image.shape
+
+
+#img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+#ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+#mask_inv = cv2.bitwise_not(mask)
+
 rows,cols,channels = resized_image.shape
 roi = resized_image[0:rows, 0:cols ]
 
@@ -20,7 +32,14 @@ cap = cv2.VideoCapture(mov_file)
 
 while(cap.isOpened()):
     ret, frame = cap.read()    
-    frame[0:resized_image.shape[0],0:resized_image.shape[1]] = resized_image
+    roi = frame[0:rows, 0:cols ]
+    img1_bg = cv2.bitwise_and(roi,roi,mask = mask)
+    img2_fg = cv2.bitwise_and(resized_image,resized_image,mask = mask_inv)
+    dst = cv2.add(img1_bg,img2_fg)
+    frame[0:rows, 0:cols ] = dst
+    #frame[0:resized_image.shape[0],0:resized_image.shape[1]] = resized_image
+    
+
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
