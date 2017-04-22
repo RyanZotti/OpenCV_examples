88ee608099f0e9b3ba10848a8c26c119e36801c2
Ryan Zotti <ryanzotti@ryans-mbp.attlocal.net>
2017-04-22 15:15:39 -0700

    Add more sources

--- color_contrast_normalization.py
+++ color_contrast_normalization.py
@@ -1,7 +1,9 @@
 import cv2
-import numpy as np
 
 # Got the code from here: http://stackoverflow.com/questions/24341114/simple-illumination-correction-in-images-opencv-c
+# Also here: http://stackoverflow.com/questions/31998428/opencv-python-equalizehist-colored-image
+# And here: https://www.packtpub.com/mapt/book/Application-Development/9781785283932/2/ch02lvl1sec26/Enhancing%20the%20contrast%20in%20an%20image
+
 img = cv2.imread('./images/ski_lift.png')
 
 img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
